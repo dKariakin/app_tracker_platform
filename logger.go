b0409da7cfdfccd87b397a443473e33d19b18ee8
Dmitry <karyakin.dmitry@gmail.com>
2023-06-22 00:10:36 +0200

    get default log level from env

--- logger.go
+++ logger.go
@@ -7,14 +7,30 @@ import (
 )
 
 var log *zap.SugaredLogger
-var defaultLvl = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+var defaultLvl zap.AtomicLevel
 
 func init() {
+	getEnvLogLvl()
 	SetLogger(New())
 }
 
+// SetLogger replaces a default logger created on the start of the application with a provided logger
 func SetLogger(logger *zap.SugaredLogger) {
 	log = logger
+}
+
+// getEnvLogLvl searches a log level in an environment variable LOG_LVL and sets it as default.
+// debug is set as default level in case if LOG_LVL is not set
+func getEnvLogLvl() {
+	lvl := "debug"
+
+	if logEnv, ok := os.LookupEnv("LOG_LVL"); ok {
+		if err := SetLogLvl(logEnv); err != nil {
+			_ = SetLogLvl(lvl)
+		}
+	} else {
+		_ = SetLogLvl(lvl)
+	}
 }
 
 // SetLogLvl sets the specified log level
@@ -31,6 +47,8 @@ func SetLogLvl(lvl string) error {
 	return err
 }
 
+// New creates a new instance of zap.SugaredLogger with json encoder, default level specified in LOG_LVL env variable
+// and a time format equals to 2006-01-02T15:04:05.000Z0700
 func New() *zap.SugaredLogger {
 	conf := zap.NewProductionEncoderConfig()
 	conf.EncodeTime = zapcore.ISO8601TimeEncoder
